97f5b7a746a7b9fd79d1971bb8b0ce4118367a1c
Mohamadhosseinzadeh <mohamad5qeh@gmail.com>
2025-09-09 07:50:16 -0700

    mohamad route

--- app.py
+++ app.py
@@ -122,7 +122,10 @@ def update(id):
         flash('There was an issue updating your task.', 'danger')
         return redirect(url_for('index'))
 
-
+@app.route('/mohamad')
+def m():
+	return 'hello mohamad'
+	
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
